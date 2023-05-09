bf2ebe05c96385687c839b2ecc9cd9dadddb6b89
John Lammon <johnlammon81@gmail.com>
2023-05-09 10:59:23 -0400

    revised commit

--- 8_ball_V2.py
+++ 8_ball_V2.py
@@ -16,3 +16,5 @@ get_answer = ["Yes", "No", "It is possible", "All signs point to yes", "All sign
 get_val = random.randint(min_val, max_val)
 
 print(get_answer[get_val])
+
+input("Press Enter to exit...")
